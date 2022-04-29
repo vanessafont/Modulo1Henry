cb0d23272634ad67eb163e4e7db8962b4f582e2d
anaharri <anaharringtonb@gmail.com>
2022-04-29 17:10:21 -0300

    Fix: consigna hw 03

--- 03-EstructuraDeDatos-I/homework/homework.js
+++ 03-EstructuraDeDatos-I/homework/homework.js
@@ -1,28 +1,33 @@
 'use strict'
-// Las funciones nFactoria y nFibonacci deben resolverlas
-// usando recursión. Una vez realizadas de esa forma pueden probar hacerlas
-// de forma iterativa pero esto último no es obligatorio.
+
+/*
+Definir las funciones recursivas nFactorial y nFibonacci.
+
+nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número natural, su factorial (representado como n!) es el producto de n por todos los números naturales menores que él y mayores a 0. Ejemplo: 5! = 5 * 4 * 3 * 2 * 1
+
+nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
+Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
+
+Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
+
+
+Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
+*/
 
 function nFactorial(n) {
-  // devolvé el factorial de n (n!)
-  // ej:
-  // el factorial de 3 es 6 (3 * 2 * 1)
 }
 
 function nFibonacci(n) {
-  // Secuencia de Fibonacci: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,…
-  // Retorna el enésimo numero de la serie
-  // nFibonacci(0) // 0  // el elemento 0 es cero
-  // nFibonacci(1) // 1 // el elemento 1 es 1
-  // nFibonacci(6) // 1 // el elemento 6 es 8
-
 }
 
-// Para esta parte no es necesario utilizar recursión.
-// Implementa la clase Queue que debe contener los siguientes métodos:
-// enqueue: Agrega un valor a la queue. Respeta el orden existente.
-// dequeue: Remueve un valor de la queue. Obedece a FIFO y respeta el underflow (devuelve undefined cuando la queue tiene size cero, o sea, cuando no tiene ningún elemento).
-// size: Devuelve el número de elementos que contiene la queue.
+/*
+Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
+  - enqueue: agrega un valor respetando el orden.
+  - dequeue: remueve un valor respetando el orden. Retorna undefined cuando la queue está vacía.
+  - size: retorna el tamaño (cantidad de elementos) de la queue.
+
+Pueden utilizar class o función constructora.
+*/
 
 function Queue() {
 
